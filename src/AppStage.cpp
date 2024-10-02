69156449fb3c39ce1afc1fde01c06475b717e8d0
Jeff Wofford <github@jeffwofford.com>
2024-10-02 13:03:17 -0400

    Emscripten: now runs.

--- src/AppStage.cpp
+++ src/AppStage.cpp
@@ -22,7 +22,7 @@ namespace
     {
         typename T::ptr t = fr::createObject< T >();
     }
-	
+
 	template< typename IterT >
 	void saveGIF( const path& path, IterT begin, IterT end, const vec2ui& dims, TimeType delaySeconds = 0, size_t framePeriod = 0, int nColors = 16 )
 	{
@@ -30,16 +30,16 @@ namespace
 		{
 			return static_cast< int >( std::round( seconds * 100 ));
 		};
-		
+
 		const auto delayCSecs = secondsToCentiseconds( delaySeconds );
-		
+
 		jo_gif_t gif = jo_gif_start( path.c_str(), dims.x, dims.y, true, nColors );
 
 		const auto writeFrame = [&]( const std::vector< unsigned char >& frameData )
 		{
 			jo_gif_frame( &gif, frameData.data(), delayCSecs, false );
 		};
-		
+
 		for( size_t i = 0; begin != end; ++begin, ++i )
 		{
 			if( framePeriod == 0 || ( i % framePeriod ) == 0 )
@@ -47,17 +47,17 @@ namespace
 				writeFrame( *begin );
 			}
 		}
-		
+
 		jo_gif_end(&gif);
 	}
 }
 
 namespace fin
-{	
+{
     void forceInclusion()
     {
         ASSERT( false );    // Don't actually call this.
-        
+
         // Reference Fresh static library functions and classes that might otherwise be stripped due to apparent non-use.
         use< ScreenEffects >();
         use< FantasyConsole >();
@@ -71,22 +71,24 @@ namespace fin
 	DEFINE_VAR( AppStage, fr::ValueControls::ptr, m_valueControls );
 	DEFINE_VAR( AppStage, fr::Sprite::ptr, m_tvOverlay );
 	DEFINE_VAR( AppStage, fr::UIPopup::ptr, m_virtualControlsGuide );
-	FRESH_IMPLEMENT_STANDARD_CONSTRUCTORS( AppStage )	
-	
+	FRESH_IMPLEMENT_STANDARD_CONSTRUCTORS( AppStage )
+
 	void AppStage::onAllLoaded()
 	{
+		trace( "Fresh.ly AppStage::onAllLoaded()" );
+
 		Super::onAllLoaded();
-		
+
 		if( m_tvOverlay )
 		{
 			m_tvOverlay->isTouchEnabled( false );
 		}
-		
+
         if( !m_consoleHost )
         {
             m_consoleHost = this;
         }
-        
+
 		// Load the editor "game".
 		//
 		if( !m_editor )
@@ -94,7 +96,7 @@ namespace fin
 			try
 			{
 				const auto editorGamePath = fr::getResourcePath( "assets/games/editor" );
-				
+
 				m_editor = createObject< FantasyConsole >();
 				m_editor->loadGame( editorGamePath );
 			}
@@ -103,9 +105,9 @@ namespace fin
 				release_trace( "Couldn't load editor game." );
 			}
 		}
-		
+
 		if( m_editor && m_editor->compiled() )
-		{			
+		{
 			activate( m_editor );
 		}
 		else
@@ -119,7 +121,7 @@ namespace fin
 			//
 			return;
 		}
-		
+
 		// Load the saved game path if it exists.
 		//
 		std::string gamePath;
@@ -134,7 +136,7 @@ namespace fin
 				release_trace( "Couldn't load game from '" << gamePath << ". Error: " << e.what() );
 			}
 		}
-		
+
 		// Else, load the autorun game if it exists.
 		//
 		if( !m_game )
@@ -151,9 +153,11 @@ namespace fin
 				release_trace( "Couldn't load autorun game: '" << e.what() << "'" );
 			}
 		}
-		
+
 		if( m_game && m_game->compiled() )
 		{
+			trace( "Activating game." );
+
 			activate( m_game );
 			loadControls();
 		}
@@ -162,11 +166,11 @@ namespace fin
 			m_game = nullptr;
 		}
 	}
-	
+
 	void AppStage::maybeShowVirtualControlsGuide( bool withHand )
 	{
 		if( !Application::instance().isMultitouch() ) return;
-		
+
 		if( m_virtualControlsGuide && m_game->supportsVirtualControls() )
 		{
 			m_virtualControlsGuide->show();
@@ -179,12 +183,12 @@ namespace fin
 //			scheduleCallback( FRESH_CALLBACK( onReadyToHideVirtualControlsGuide ), 8.0 );
 		}
 	}
-	
+
 	std::string AppStage::gameSaveKey( const fr::path& gamePath ) const
 	{
 		return createString( "save_" << gamePath.stem() );
 	}
-	
+
 	void AppStage::onAppMayTerminate()
 	{
 		// Save the path to the existing game.
@@ -192,7 +196,7 @@ namespace fin
 		if( m_game )
 		{
 			savePreference( "gamePath", m_game->gamePath().string() );
-			
+
 			const auto memento = m_game->saveMemento();
 			savePreference( gameSaveKey( m_game->gamePath() ), memento );
 		}
@@ -215,7 +219,7 @@ namespace fin
 				return true;
 			}
 		};
-		
+
 		switch( event.key() )
 		{
 			case Keyboard::Key::R:
@@ -240,7 +244,7 @@ namespace fin
 				{
 					return true;
 				}
-				
+
 			case Keyboard::Key::Escape:
 				if( down )
 				{
@@ -290,7 +294,7 @@ namespace fin
 				}
 				m_pastScreenshots.emplace_back( std::move( texels ));
 				const size_t excessFrames = m_pastScreenshots.size() - m_maxGIFFrames;
-				
+
 				if( excessFrames < m_maxGIFFrames )
 				{
 					m_pastScreenshots.erase( m_pastScreenshots.begin(), m_pastScreenshots.begin() + excessFrames );
@@ -307,14 +311,14 @@ namespace fin
 				const auto fileName = createString( "Freshly Shot " << getStandardTimeDisplay( std::time( NULL )) << ".png" );
 				const auto directory = getDocumentPath( "shots" );
 				create_directories( directory );
-				
+
 				const auto path = directory / fileName;
 				lodepng::encode( path.string(), m_pastScreenshots.back(), m_screenshotsDims.x, m_screenshotsDims.y );
-				
+
 				m_editor->console( createString( "Screen shot taken: " << path ));
 			}
 		}
-		
+
 		if( m_pendingSaveGIF )
 		{
 			m_pendingSaveGIF = false;
@@ -323,7 +327,7 @@ namespace fin
 				const auto fileName = createString( "Freshly GIF " << getStandardTimeDisplay( std::time( NULL )) << ".gif" );
 				const auto directory = getDocumentPath( "shots" );
 				create_directories( directory );
-				
+
 				const auto path = directory / fileName;
 				const size_t framePeriod = 4;
 				saveGIF( path, m_pastScreenshots.begin(), m_pastScreenshots.end(), m_screenshotsDims, framePeriod / 60.0, framePeriod );
@@ -331,19 +335,19 @@ namespace fin
 				m_editor->console( createString( "GIF saved: " << path ));
 			}
 		}
-		
+
         if( m_pendingReload )
         {
             m_pendingReload = false;
             reloadGame();
         }
-        
+
         if( m_pendingActivate )
 		{
 			activate( m_pendingActivate );
 			m_pendingActivate = nullptr;
 		}
-		
+
 		if( m_valueControls )
 		{
 			m_valueControls->console( m_game );
@@ -354,14 +358,14 @@ namespace fin
 			// For some reason I have to flip this in Y.
 			m_tvOverlay->scale( vec2( 1.0f, -1.0f ) * vector_cast< real >( m_activeConsole->screenDims() ) / vector_cast< real >( m_tvOverlay->texture()->getOriginalDimensions() ));
 		}
-		
+
 		if( auto fpsDisplay = getDescendantByName<TextField>( "fpsDisplay" ))
 		{
 			const TimeType deltaTimeSeconds = stage().lastFrameDurationReal();
 			if( deltaTimeSeconds > 0.0 )
 			{
 				const TimeType fps = 1.0 / deltaTimeSeconds;
-				
+
 				m_smoothedFPS = lerp( m_smoothedFPS, fps, 0.25 );
 
 				fpsDisplay->text( createString( std::fixed << std::setfill( ' ' ) << std::setprecision( 0 ) << std::setw( 6 ) << m_smoothedFPS ));
@@ -371,7 +375,7 @@ namespace fin
 				fpsDisplay->text( "" );
 			}
 		}
-		
+
 		if( m_virtualControlsGuide )
 		{
 			if( !Application::instance().isMultitouch() )
@@ -389,10 +393,10 @@ namespace fin
 				}
 			}
 		}
-		
-		Super::update();		
+
+		Super::update();
 	}
-	
+
 	void AppStage::render( TimeType relativeFrameTime, RenderInjector* injector )
 	{
 		Super::render( relativeFrameTime, injector );
@@ -402,15 +406,15 @@ namespace fin
 	{
 		const auto game = createObject< FantasyConsole >();
 		game->addEventListener( fr::FantasyConsole::MESSAGE, FRESH_CALLBACK( onGameConsoleMessage ));
-		
+
 		game->loadGame( gamePath );
 
 		if( game->compiled() )
 		{
 			restoreSavedGame( *game );
-			
+
 			loadControls();
-			
+
 			m_game = game;
 			m_pendingActivate = m_game;
 		}
@@ -419,13 +423,13 @@ namespace fin
 			m_editor->console( createString( "Unable to compile game at '" << gamePath << "'." ));
 		}
 	}
-	
+
 	void AppStage::reloadGame( bool reset )
 	{
 		if( m_game )
 		{
 			m_game->reload();
-			
+
 			if( m_game->compiled() )
 			{
 				if( !reset )
@@ -441,11 +445,11 @@ namespace fin
 			m_editor->console( "No game loaded. Use `load <game path>`." );
 		}
 	}
-	
+
 	void AppStage::restoreSavedGame( fr::FantasyConsole& console )
 	{
 		ASSERT( console.compiled() );
-		
+
 		std::string memento;
 		if( loadPreference( gameSaveKey( console.gamePath() ), memento ))
 		{
@@ -457,7 +461,7 @@ namespace fin
 	{
 		m_pendingTakeScreenshot = false;
 		m_pendingSaveGIF = false;
-		
+
 		// Deactivate the prior active console.
 		//
 		if( m_activeConsole )
@@ -478,20 +482,24 @@ namespace fin
 				console->removeEventListener( fr::FantasyConsole::VIRTUAL_TRACKBALL_USED, FRESH_CALLBACK( onVirtualTrackballUsed ));
 			}
 
-			m_virtualControlsGuide->visible( console == m_game );
+			if( m_virtualControlsGuide )
+			{
+				m_virtualControlsGuide->visible( console == m_game );
+			}
 
 			if( m_consoleHost->hasChild( m_activeConsole ))
 			{
 				m_consoleHost->removeChild( m_activeConsole );
 			}
-			
+
 			m_activeConsole->activated( false );
-			
+
 			m_activeConsole = nullptr;
 		}
-		
+
 		if( console )
 		{
+			release_trace("Adding console to host.");
 			m_consoleHost->addChild( console );
 
 			if( console == m_editor )
@@ -503,15 +511,15 @@ namespace fin
 				console->addEventListener( fr::FantasyConsole::VIRTUAL_CONTROLS_SHOW, FRESH_CALLBACK( onVirtualControlsShow ));
 				console->addEventListener( fr::FantasyConsole::VIRTUAL_CONTROLS_HIDE, FRESH_CALLBACK( onVirtualControlsHide ));
 				console->addEventListener( fr::FantasyConsole::VIRTUAL_TRACKBALL_USED, FRESH_CALLBACK( onVirtualTrackballUsed ));
-				
+
 				maybeShowVirtualControlsGuide( true );
 			}
-			
+
 			console->activated( true );
 		}
-		
+
 		m_activeConsole = console;
-		
+
 		if( m_valueControls )
 		{
 			m_valueControls->active( m_activeConsole == m_game );
@@ -522,30 +530,30 @@ namespace fin
 	{
 		return getDocumentPath( change_extension( m_game->gamePath(), "txt" ));
 	}
-	
+
 	void AppStage::loadControls()
 	{
 		if( m_game && m_valueControls )
 		{
 			m_valueControls->clear();
-			
+
 			const auto controlsPath = gameControlSavePath();
-			
+
 			if( exists( controlsPath ))
 			{
 				const std::string state = getFileText( controlsPath );
-				
+
 				m_valueControls->restoreState( state );
 			}
 		}
 	}
-	
+
 	void AppStage::saveControls() const
 	{
-		if( m_game )
+		if( m_game && m_valueControls)
 		{
 			const auto state = m_valueControls->saveState();
-			
+
 			std::ofstream out( gameControlSavePath().c_str() );
 			out << state;
 		}
@@ -619,9 +627,9 @@ namespace fin
 						m_editor->console( "Usage: `load <game path>`" );
 						return;
 					}
-					
+
 					const auto& gamePath = arguments[ 1 ];
-					
+
 					try
 					{
 						loadGame( gamePath );
@@ -658,124 +666,139 @@ namespace fin
 			},
             { "inspect", [&]( const std::vector< std::string >& arguments )
                 {
-					const auto parseReal = []( const std::string& s, real defaultValue = 0 ) {
-						real n = defaultValue;
-						std::istringstream str( s );
-						str >> n;
-						return n;
-					};
-					
-					const auto parseType = []( const std::string& s ) {
-						if( s == "bool" )
-						{
-							return ValueControlOptions::Type::Bool;
-						}
-						else if( s == "real" )
-						{
-							return ValueControlOptions::Type::Real;
-						}
-						else
-						{
-							return ValueControlOptions::Type::Detect;
-						}
-					};
-					
-                    if( arguments.size() < 2 )
-                    {
-                        m_editor->console( "Usage: `inspect <variable-path> [min] [max] [<type='bool'|'real'>]`" );
-                        return;
-                    }
-					
-					ValueControlOptions options;
-					
-					size_t arg = 1;
-					options.variablePath = arguments[ arg++ ];
-					options.min = arguments.size() > arg ? parseReal( arguments[ arg++ ], 0 ) : 0;
-					
-					const auto defaultMax = options.min != 0 ? options.min + 1.0f : 0.0f;
-					options.max = arguments.size() > arg ? parseReal( arguments[ arg++ ], defaultMax ) : defaultMax;
+					if( m_valueControls )
+					{
+						const auto parseReal = []( const std::string& s, real defaultValue = 0 ) {
+							real n = defaultValue;
+							std::istringstream str( s );
+							str >> n;
+							return n;
+						};
 
-					options.type = arguments.size() > arg ? parseType( arguments[ arg++ ] ) : ValueControlOptions::Type::Detect;
+						const auto parseType = []( const std::string& s ) {
+							if( s == "bool" )
+							{
+								return ValueControlOptions::Type::Bool;
+							}
+							else if( s == "real" )
+							{
+								return ValueControlOptions::Type::Real;
+							}
+							else
+							{
+								return ValueControlOptions::Type::Detect;
+							}
+						};
 
-					m_valueControls->add( options, m_game );
-					
-					saveControls();
+						if( arguments.size() < 2 )
+						{
+							m_editor->console( "Usage: `inspect <variable-path> [min] [max] [<type='bool'|'real'>]`" );
+							return;
+						}
+
+						ValueControlOptions options;
+
+						size_t arg = 1;
+						options.variablePath = arguments[ arg++ ];
+						options.min = arguments.size() > arg ? parseReal( arguments[ arg++ ], 0 ) : 0;
+
+						const auto defaultMax = options.min != 0 ? options.min + 1.0f : 0.0f;
+						options.max = arguments.size() > arg ? parseReal( arguments[ arg++ ], defaultMax ) : defaultMax;
+
+						options.type = arguments.size() > arg ? parseType( arguments[ arg++ ] ) : ValueControlOptions::Type::Detect;
+
+						m_valueControls->add( options, m_game );
+
+						saveControls();
+					}
                 }
             },
 			{ "nab", [&]( const std::vector< std::string >& arguments )
 				{
-					if( arguments.size() < 1 )
+					if( m_valueControls )
 					{
-						m_editor->console( "Usage: `nab`" );
-						return;
-					}
+						if( arguments.size() < 1 )
+						{
+							m_editor->console( "Usage: `nab`" );
+							return;
+						}
 
-					copyToPasteboard( m_valueControls->valuesString() );
-					
-					m_editor->console( "Copied to clipboard." );
+						copyToPasteboard( m_valueControls->valuesString() );
+
+						m_editor->console( "Copied to clipboard." );
+					}
 				}
 			},
             { "resetvar", [&]( const std::vector< std::string >& arguments )
 				{
-                    if( arguments.size() < 2 )
-                    {
-                        m_editor->console( "Usage: `inspect <variable-path>|#<index>`" );
-                        return;
-                    }
-					
-					// Number?
-					if( arguments[ 1 ].empty() == false && arguments[ 1 ][ 0 ] == '#' )
+					if( m_valueControls )
 					{
-						const std::string numericPart( arguments[ 1 ].begin() + 1, arguments[ 1 ].end() );
-						const int index = parseInt( numericPart, -1 );
-						m_valueControls->reset( index - 1 );
+						if( arguments.size() < 2 )
+						{
+							m_editor->console( "Usage: `inspect <variable-path>|#<index>`" );
+							return;
+						}
+
+						// Number?
+						if( arguments[ 1 ].empty() == false && arguments[ 1 ][ 0 ] == '#' )
+						{
+							const std::string numericPart( arguments[ 1 ].begin() + 1, arguments[ 1 ].end() );
+							const int index = parseInt( numericPart, -1 );
+							m_valueControls->reset( index - 1 );
+						}
+						else
+						{
+							m_valueControls->reset( arguments[ 1 ] );
+						}
+
+						saveControls();
 					}
-					else
-					{
-						m_valueControls->reset( arguments[ 1 ] );
-					}
-					
-					saveControls();
 				}
 			},
 			{ "forget", [&]( const std::vector< std::string >& arguments )
 				 {
-					 if( arguments.size() < 1 )
-					 {
-						 m_editor->console( "Usage: `forget <variable-path>|#<index=last>`" );
-						 return;
-					 }
-					 
-					 // Number?
-					 if( arguments.size() == 1 )
-					 {
-						 m_valueControls->removeLast();
-					 }
-					 else if( arguments[ 1 ].empty() == false && arguments[ 1 ][ 0 ] == '#' )
-					 {
-						 const std::string numericPart( arguments[ 1 ].begin() + 1, arguments[ 1 ].end() );
-						 const int index = parseInt( numericPart, -1 );
-						 m_valueControls->remove( index - 1 );
-					 }
-					 else
-					 {
-						 m_valueControls->remove( arguments[ 1 ] );
-					 }
-					 
-					 saveControls();
+					if( m_valueControls )
+					{
+						if( arguments.size() < 1 )
+						{
+							m_editor->console( "Usage: `forget <variable-path>|#<index=last>`" );
+							return;
+						}
+
+						// Number?
+						if( arguments.size() == 1 )
+						{
+							m_valueControls->removeLast();
+						}
+						else if( arguments[ 1 ].empty() == false && arguments[ 1 ][ 0 ] == '#' )
+						{
+							const std::string numericPart( arguments[ 1 ].begin() + 1, arguments[ 1 ].end() );
+							const int index = parseInt( numericPart, -1 );
+							m_valueControls->remove( index - 1 );
+						}
+						else
+						{
+							m_valueControls->remove( arguments[ 1 ] );
+						}
+
+						saveControls();
+					}
 				 }
 			},
 			{ "forgetall", [&]( const std::vector< std::string >& arguments )
 				 {
-					 if( arguments.size() < 1 )
-					 {
-						 m_editor->console( "Usage: `forgetall`" );
-						 return;
-					 }
-					 
-					 m_valueControls->clear();
-					 
-					 saveControls();
+					if( m_valueControls )
+					{
+						if( arguments.size() < 1 )
+						{
+							m_editor->console( "Usage: `forgetall`" );
+							return;
+						}
+
+						m_valueControls->clear();
+
+						saveControls();
+					}
 				 }
 			},
             { "call", [&]( const std::vector< std::string >& arguments )
@@ -785,7 +808,7 @@ namespace fin
                         m_editor->console( "Usage: `call <method>`" );
                         return;
                     }
-                    
+
                     const auto& methodName = arguments.front();
                     m_editor->callScriptFunction( methodName );
                 }
@@ -829,17 +852,17 @@ namespace fin
 			m_editor->console( event.message() );
 		}
 	}
-	
+
 	FRESH_DEFINE_CALLBACK( AppStage, onVirtualControlsShow, fr::Event )
 	{
 		maybeShowVirtualControlsGuide( false );
 	}
-	
+
 	FRESH_DEFINE_CALLBACK( AppStage, onVirtualControlsHide, fr::Event )
 	{
 		onReadyToHideVirtualControlsGuide( event );
 	}
-	
+
 	FRESH_DEFINE_CALLBACK( AppStage, onVirtualTrackballUsed, fr::Event )
 	{
 		if( m_virtualControlsGuide )
